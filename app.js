29c9df921b197e9ea7df079092d7e82797d4f377
nguyenca03 <nguyencak4d@gmail.com>
2016-06-16 16:07:58 +0700

    First Commit

---
+++ app.js
@@ -0,0 +1,24 @@
+var myApp = angular.module('myApp', []);
+
+myApp.controller('mainController', ['$scope', '$filter', '$timeout', function($scope, $filter, $timeout) {
+    
+    $scope.abc = '';
+    
+    $scope.lowercasehandle = function() {
+        return $filter('lowercase')($scope.abc);
+    };
+    
+    $scope.$watch('abc', function(newValue, oldValue) {
+        
+        console.info('Changed!');
+        console.log('Old:' + oldValue);
+        console.log('New:' + newValue);
+        
+    });
+    
+     $timeout(function() {
+        $scope.abc = 'newtwitterhandle';
+        console.log('Scope changed!');    
+     }, 3000);
+    
+}]);
